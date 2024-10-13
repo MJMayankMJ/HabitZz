8d1219f3ad13d4fb0d05848b33185b2af79c73a7
MJMayankMJ <226301131@gkv.ac.in>
2024-10-13 11:24:43 +0530

    Initial Commit

---
+++ HabitZz/HabitZzApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitZzApp.swift
+//  HabitZz
+//
+//  Created by Mayank Jangid on 10/13/24.
+//
+
+import SwiftUI
+
+@main
+struct HabitZzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HabitZzTests/HabitZzTests.swift
@@ -0,0 +1,17 @@
+//
+//  HabitZzTests.swift
+//  HabitZzTests
+//
+//  Created by Mayank Jangid on 10/13/24.
+//
+
+import Testing
+@testable import HabitZz
+
+struct HabitZzTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
